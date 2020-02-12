4496a3ddcf40e825c1eb3840bc0b0879c383cd96
aidentab <33820269+aidentab@users.noreply.github.com>
2020-02-12 14:58:18 -0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-//const { Client, Collection } = require("discord.js");
-//const client = new Client();
-//var fs = require("fs");
-//var path = require('path');
+const { Client, Collection } = require("discord.js");
+const client = new Client();
+var fs = require("fs");
+var path = require('path');
 
   ["aliases", "commands"].forEach(x => (client[x] = new Collection()));
   ["command", "events"].forEach(x => require(`./handlers/${x}`)(client));
